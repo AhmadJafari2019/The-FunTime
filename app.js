d90b95f82961b7d279367c533639c78ec1570df0
Ahmad <ahmad.hazara@gmx.ch>
2019-10-16 17:52:01 +0200

    Update the round Score for each Player

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
-
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 // Setting an event handler
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // Random Number.
@@ -15,7 +19,23 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
+    // Update the round score if the rolled Number was not a 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player.
+        roundScore = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
 
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
 
 
 });
