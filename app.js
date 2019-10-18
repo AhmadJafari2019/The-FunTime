5610d061ac295aff6f64323f5979fbb9aa5b5815
Ahmad <ahmad.hazara@gmx.ch>
2019-10-18 15:10:51 +0200

    Using the function nextPlayer to avoid reapiting the Code.

--- app.js
+++ app.js
@@ -27,16 +27,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector("#current-" + activePlayer).textContent = roundScore;
     } else {
         // Next Player.
-        roundScore = 0;
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
 
     }
 });
@@ -44,8 +35,21 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer] += roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    nextPlayer();
 });
 
+function nextPlayer() {
+    roundScore = 0;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
